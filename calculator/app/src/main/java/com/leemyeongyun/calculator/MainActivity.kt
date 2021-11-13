efccde7802071e3c8e83070451ed5860a4f93c22
mangyun <qk1890@naver.com>
2021-11-14 00:18:32 +0900

    계산기 어플

---
+++ calculator/app/src/main/java/com/leemyeongyun/calculator/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.leemyeongyun.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //인자를 view로 받아 클릭함수 구현
+    fun buttonClicked(v: View){
+
+    }
+    fun resultButtonClicked(v: View){
+
+    }
+    fun historyButtonClicked(v: View){
+
+    }
+    fun clearButtonClicked(v: View){
+
+    }
+}
