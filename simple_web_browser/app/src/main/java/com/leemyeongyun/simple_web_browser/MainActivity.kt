abea7ec465b6af96db7c28b194b52c501e2c63c8
mangyun <qk1890@naver.com>
2021-11-25 15:10:53 +0900

    심플 웹 브라우저 최종

--- simple_web_browser/app/src/main/java/com/leemyeongyun/simple_web_browser/MainActivity.kt
+++ simple_web_browser/app/src/main/java/com/leemyeongyun/simple_web_browser/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.leemyeongyun.simple_web_browser
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import android.webkit.URLUtil
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.core.widget.ContentLoadingProgressBar
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
@@ -36,6 +40,10 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.webView)
     }
 
+    private val progressBar: ContentLoadingProgressBar by lazy {
+        findViewById(R.id.progressBar)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,9 +68,8 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetJavaScriptEnabled")//경고 무시
     private fun initViews() {
         webView.apply {
-
-
             webViewClient = WebViewClient() // webView에 load하게 함
+            webChromeClient = WebChromeClient()
             settings.javaScriptEnabled = true //자바스크립트의 보안으로 인해 webView에 추가해야 사이트 기능을 이용할 수 있음
             loadUrl(DEFAULT_URL)
         }
@@ -75,7 +82,11 @@ class MainActivity : AppCompatActivity() {
 
         addressBar.setOnEditorActionListener { v, actionId, event ->//주소창 입력액션
             if (actionId == EditorInfo.IME_ACTION_DONE) {//키보드가 닫히면
-                webView.loadUrl(v.text.toString())//해당 사이트로 이동
+                val loadingUrl = v.text.toString() //주소를 받음
+                if (URLUtil.isNetworkUrl(loadingUrl)) { // URLUtil로 앞에 http가 붙어있다면,
+                    webView.loadUrl(loadingUrl)//해당 사이트로 이동
+                } else// 없다면 http를 붙임
+                    webView.loadUrl("http://$loadingUrl") // https를 지원하는 브라우저들은 http로 접근해도 https로 리다이렉팅이 기본임
             }
             return@setOnEditorActionListener false //true를 반환하게된다면 액션을 진행할 수가 없어서 키보드가 안닫아짐
 
@@ -99,10 +110,31 @@ class MainActivity : AppCompatActivity() {
 
     //새로고침 표시 제거
     inner class WebViewClient : android.webkit.WebViewClient() { //inner로 상위 클래스 접근가능
+
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            progressBar.show() //페이지가 시작될때 보여주게 처리
+        }
+
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
 
             refreshLayout.isRefreshing = false
+            progressBar.hide() //로딩이 완료되면 사라지게 처리
+            goBackButton.isEnabled = webView.canGoBack() //뒤 페이지로 갈 수 있다면 누를 수 있게함.
+            goForwardButton.isEnabled = webView.canGoForward() //앞 페이지로 갈 수 있다면 누를 수 있게함.
+            addressBar.setText(url) // 로딩된 url 보여줌
+        }
+    }
+
+    //브라우저 차원의 복잡한 이벤트를 처리하려면 WebChromeClient 이용
+    inner class WebChromeClient : android.webkit.WebChromeClient() {
+
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            progressBar.progress = newProgress // 로딩된 정도의 기본값, 0 ~ 100 그대로 저장
         }
     }
 
